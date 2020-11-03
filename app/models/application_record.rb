708abbfe6b8e7840a0cd3dfa37d6c6093ddeabd6
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-03 19:30:26 +0900

    ユーザー認証機能の実装の続き

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    added_attrs = [ :nickname, :email, :password, :birthday ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-  end
 end
