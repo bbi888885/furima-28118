8d1f688dbcf699e8e2bfb7f4f154fdc1b8923456
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-03 18:57:12 +0900

    ユーザー情報の制限の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, 
+                    length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+                    
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX },
+                       presence: true, length: { minimum: 6 },
+                       confirmation: true
+
 end
