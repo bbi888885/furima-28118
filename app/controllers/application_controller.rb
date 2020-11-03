6e645a8462fe9691a1dd8ecef76f96a355a87c6b
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-03 21:37:58 +0900

    登録情報をモデル上に保存させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
   private
   def configure_permitted_parameters
-    added_attrs = [ :lastname, :firstname, :furigana_lastname, :furigana_firstname, 
-                  :email, :password, :nickname, :birthday ]
-    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname, :firstname, :furigana_lastname, :furigana_firstname, 
+      :email, :password, :nickname, :birthday])
   end
 
 
@@ -15,4 +16,6 @@ class ApplicationController < ActionController::Base
     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
+
+  
 end
