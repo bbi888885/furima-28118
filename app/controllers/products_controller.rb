ee31f324a040d71691ff2e851b5bdee4a634f7d4
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-05 22:06:38 +0900

    商品詳細の保存機能を実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,19 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:item, :description, :category_id, :status_id, :price,
+                                    :delivery_cost_id, :shipping_area_id, :how_many_days_id, :user_id)
+  end
+
 end
