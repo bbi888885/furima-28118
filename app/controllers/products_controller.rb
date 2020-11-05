85eb171d7249bb8df9d08341184357621ae0d74c
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-05 23:29:42 +0900

    priceのバリデーションの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,8 +13,7 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:image, :item, :description, :category_id, :status_id, :price,
-                                    :delivery_cost_id, :shipping_area_id, :how_many_days_id,).
-                                    merge(user_id: current_user.id)
+                                    :delivery_cost_id, :shipping_area_id, :how_many_days_id, :user_id)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,14 @@ class Product < ApplicationRecord
   belongs_to :categories
   has_one_attached :image
 
-  validates :categories_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
   validates :delivery_cost_id, numericality: { other_than: 1 }
   validates :shipping_area_id, numericality: { other_than: 1 }
   validates :how_many_days_id, numericality: { other_than: 1 }
-
-
+  validates :price, :numericality => { :greater_than_or_equal_to => 300,
+                                       :less_than_or_equal_to => 9999999,
+                                     }
 
   
 end
