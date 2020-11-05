7e235ba263e56f75fc7fe726aded5861fbc8860b
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-05 21:33:05 +0900

    Productsテーブルの修正

--- db/migrate/20201105122242_create_products.rb
+++ db/migrate/20201105122242_create_products.rb
@@ -1,15 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string             :item,              null: false
-      t.text               :description,       null: false
-      t.category_id        :integer,           null: false
-      t.status_id          :integer,           null: false
-      t.price              :integer,           null: false
-      t.delivery_cost_id   :integer,           null: false
-      t.shipping_area_id   :integer,           null: false
-      t.how_many_days_id   :integer,           null: false
-      t.user               :references,        null: false, foreign_key: true
+      t.string       :item,              null: false
+      t.text         :description,       null: false
+      t.integer      :category_id,       null: false
+      t.integer      :status_id,         null: false
+      t.integer      :price,             null: false
+      t.integer      :delivery_cost_id,  null: false
+      t.integer      :shipping_area_id,  null: false
+      t.integer      :how_many_days_id,  null: false
+      t.references   :user,              null: false, foreign_key: true
       t.timestamps
     end
   end
