52341de003aadd422619edbdd0d667d642332189
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-05 23:50:44 +0900

    販売価格は半角数字のみ保存可能の設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,9 +8,10 @@ class Product < ApplicationRecord
   validates :delivery_cost_id, numericality: { other_than: 1 }
   validates :shipping_area_id, numericality: { other_than: 1 }
   validates :how_many_days_id, numericality: { other_than: 1 }
-  validates :price, :numericality => { :greater_than_or_equal_to => 300,
-                                       :less_than_or_equal_to => 9999999,
-                                     }
+  validates :price, format: { with: /\A[0-9]+\z/ }
+            :numericality => { :greater_than_or_equal_to => 300,
+                               :less_than_or_equal_to => 9999999,
+                             }
   validates :image, presence: true
 
   
