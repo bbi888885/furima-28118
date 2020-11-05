cfae8910ee06c2a346c59f7d4011d33463fb31d9
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-05 23:03:02 +0900

    statusモデルの定義とバリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,6 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   validates :categories_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
 
 end
