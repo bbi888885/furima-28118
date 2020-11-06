5fedb5a43d2e55961897a945935956180315f422
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-06 22:16:59 +0900

    エラー解消

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,16 @@ class ProductsController < ApplicationController
   end
 
   def new
-
     @product = Product.new
   end
 
   def create
-    Product.create(product_params)
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
-  belongs_to :categories
   has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1 }
@@ -8,9 +7,10 @@ class Product < ApplicationRecord
   validates :delivery_cost_id, numericality: { other_than: 1 }
   validates :shipping_area_id, numericality: { other_than: 1 }
   validates :how_many_days_id, numericality: { other_than: 1 }
-  validates :price, format: { with: /\A[0-9]+\z/ }
-            :numericality => { :greater_than_or_equal_to => 300,
-                               :less_than_or_equal_to => 9999999,
+  validates :price, format: { with: /\A[0-9]+\z/ },
+            :numericality => { 
+              :greater_than_or_equal_to => 300,
+              :less_than_or_equal_to => 9999999,
                              }
   validates :image, presence: true
 
