cc3378bd693656bfd1a14482db6a7e91fb3fdfc3
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-07 18:50:19 +0900

    メンターのアドバイスに沿って修正しました

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,17 +2,21 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :category_id, numericality: { other_than: 1, message: "can not be blank" }
-  validates :status_id, numericality: { other_than: 1, message: "can not be blank" }
-  validates :delivery_cost_id, numericality: { other_than: 1, message: "can not be blank" }
-  validates :shipping_area_id, numericality: { other_than: 1, message: "can not be blank" }
-  validates :how_many_days_id, numericality: { other_than: 1, message: "can not be blank" }
+  with_options numericality: { other_than: 1, message: "can not be blank" } do
+    validates :category_id 
+    validates :status_id
+    validates :delivery_cost_id
+    validates :shipping_area_id
+    validates :how_many_days_id
+  end
   validates :price, format: { with: /\A[0-9]+\z/ },
                     numericality: {
                       greater_than_or_equal_to: 300,
                       less_than_or_equal_to: 9_999_999
                     }
-  validates :image, presence: true
-  validates :item, presence: true
-  validates :description, presence: true
+  with_options presence: true do
+    validates :image
+    validates :item
+    validates :description
+  end
 end
