a4d382d6e5e0c315b7a90d3c2377c6f45ec845cf
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-08 22:10:09 +0900

    updateアクションの編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,11 @@ class ProductsController < ApplicationController
 
   def update
     product = Product.find(params[:id])
-    product.update(product_params)
+    if product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
   end
 
 
