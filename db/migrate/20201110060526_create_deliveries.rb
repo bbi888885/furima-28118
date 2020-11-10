d5e35342e4659d22a5e6f98504ca559c2fb85ddd
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-10 15:08:37 +0900

    マイグレーションファイルの編集

--- db/migrate/20201110060526_create_deliveries.rb
+++ db/migrate/20201110060526_create_deliveries.rb
@@ -1,7 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
-
+      t.string     :postal_code,      null: false
+      t.integer    :prefecture_id,    null: false
+      t.string     :city,             null: false 
+      t.string     :banchi,           null: false
+      t.string     :building_name,    null: false
+      t.string     :tel,              null: false
+      t.references :record,           null: false, foreign_key: true
       t.timestamps
     end
   end
