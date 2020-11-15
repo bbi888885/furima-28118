d526c5e887d9d0602f64dcd2972d08e57a6a2194
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2020-11-15 18:38:00 +0900

    フォームの情報を保存できるようにする

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,27 @@
 class RecordsController < ApplicationController
+
+  def index
+    @delivery_record = DeliveryRecord.new 
+    @product = Product.find(params[:product_id])
+  end
+
+  def create
+    @delivery_record = DeliveryRecord.new(record_params)
+    if @delivery_record.valid?
+      @delivery_record.save
+      return redirect_to action: :index
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:delivery_record).permit(:user.id, :product.id, :postal_code, :prefecture_id,
+                                            :city, :banchi, :building_name, :tel)
+  end
+
+  
+
 end
