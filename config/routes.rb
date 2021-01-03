d1d1bc3371cf71e8ca1b5cdf649c4b1899d4bd0b
bbi888885 <66506461+bbi888885@users.noreply.github.com>
2021-01-03 14:21:16 +0900

    ルーティングの設定（ネスト）

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :products do
     resources :records, only: [:index, :new, :create] 
+    resources :comments, only: :create
   end
 
   
